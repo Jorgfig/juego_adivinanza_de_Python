a71c75abbbc77a8ba8478be8277ebcf1cc26ae32
JorgeFigueroa <figueroajorgear@gmail.com>
2024-08-07 15:41:56 -0300

    Juego de adivinanza en Pyhton

---
+++ juego_adivinanza.py
@@ -0,0 +1,32 @@
+
+
+import random
+
+50
+numero_secreto = random.randint (1,100)
+cant_intentos = 0
+cant_max_intentos = 5
+adivinado = False
+
+print ("¡Bienvenido al juego de adividar el número secreto. Tienes 5 intentos!")
+
+
+while not adivinado and cant_intentos < cant_max_intentos:
+    entrada = input ("Introduce un número del 1 al 99: ")
+    numero = int(entrada)
+
+    if numero == numero_secreto:
+        print ("¡Felicitaciones has adivinado el número secreto!")
+        adivinado = True
+    elif numero <  numero_secreto:
+        print ("El número es mayor al ingresado!")
+
+    else:
+        print("El número es menor al ingresado")
+    
+    cant_intentos +=1
+
+
+if not cant_intentos < cant_max_intentos:
+    print ("¡Game Over! Has superado la cantidad máxima de intentos")
+
